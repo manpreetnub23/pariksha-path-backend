b7a36b29de91895b5f45b3cb1206ba9a6e6cffe0
Singhoustic <singhgurpreet1013@gmail.com>
2025-08-23 14:38:02 +0530

    main.py file updated.

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import uvicorn
+from app.main import app  # Import the FastAPI app
 
+# Optional: keep local dev running support
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+
+
+# if __name__ == "__main__":
+#     uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
