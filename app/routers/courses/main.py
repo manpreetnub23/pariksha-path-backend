be70b2a9b97162437fcaa6e2b6ed8ddb5c5a52e0
Meownav <manavchauhan124@gmail.com>
2025-09-29 15:31:19 +0530

    Mocks being submitted now.

--- app/routers/courses/main.py
+++ app/routers/courses/main.py
@@ -9,6 +9,7 @@ from .crud import router as crud_router
 from .sections import router as sections_router
 from .materials import router as materials_router
 from .mock_tests import router as mock_tests_router
+from .mock_history import router as mock_history_router
 
 # Create the main router that includes all sub-routers
 router = APIRouter()
@@ -18,3 +19,4 @@ router.include_router(crud_router)
 router.include_router(sections_router)
 router.include_router(materials_router)
 router.include_router(mock_tests_router)
+router.include_router(mock_history_router)
